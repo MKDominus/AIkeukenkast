6f9d236614072736b311d03f893644c7077fce56
pijlD <danielpijl2001@gmail.com>
2026-04-20 14:09:23 +0200

    debug commit

--- api/obj/Debug/net10.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net10.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f122e91dc5e44af5268bc6e3c20b656372ccb94d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54c257c715863b2f40f46aed93f65bcc5a06209f")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
