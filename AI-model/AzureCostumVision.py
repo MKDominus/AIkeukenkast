9c5816b37c7bd5681448d4e61f61b7631dc4f8f0
Yoelhu <144608986+Yoelhu@users.noreply.github.com>
2026-06-15 15:26:24 +0200

    lil funny

--- AI-model/AzureCostumVision.py
+++ AI-model/AzureCostumVision.py
@@ -107,7 +107,7 @@ def get_predict(prediction, file_name, content_type, image_bytes):
     list_predictions = []
 
     for obj in prediction.predictions:
-        if obj.probability >= 0.6:
+        if obj.probability >= 0.7:
             product = {
                 "product name": obj.tag_name,
                 "probability": obj.probability
